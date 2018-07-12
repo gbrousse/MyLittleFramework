97441bc2cf2b3188a91e3c24227889a1c7a593ba
gbrousse <pro@gregory-brousse.fr>
2018-07-12 22:11:15 +0200

    Cleaning

--- index.php
+++ index.php
@@ -6,20 +6,14 @@ use Core\Common\Logs\Logger;
 use Core\Common\Conf\Config;
 use Core\Common\Db\DbPDO;
 use Core\Router\Router;
-use Core\Models\ConnectedUserModel;
-
+use Psr\Log\LogLevel;
+use Core\Common\Logs\handlers\FileHandler;
 
 session_start();
 
 
 
 $autoloader = require_once 'vendor/autoload.php';
-/*$autoloader->loadClass("Core\Common\container\ServiceLocator");
-
-print_r($autoloader);
-exit(2);*/
-
-
 
 // Chargement du conteneur de services
 $appServices = new ServiceLocator();
@@ -28,17 +22,12 @@ $appServices = new ServiceLocator();
 $config = new config ('./src/conf/config.ini');
 
 // Chargement et initialisation du logger
-$logger = new Logger($config->logs->path, array(
-    'lvl'   =>  $config->logs->level,
-    'mod'   =>  $config->logs->mod
-));
+$logger = new Logger($config->logs->level, $config->logs->backtrace);
+$fileHandler = new FileHandler($config->logs->path);
+$logger->attachHandler($fileHandler,'logfile');
 
 // Initialisation de la bdd
 $db = new DbPDO($config->bdd, $logger);
-
-// Chargement de l'utilisateur
-$user = new ConnectedUserModel($db, $logger);
-
 
 // Initialisation du router
 $router = new router($config->routes,$logger);
